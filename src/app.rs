3f2bf0d7d5bba7799a80679be9d891861d1287ee
dave-wathen <dave@wathenweb.com>
2023-05-30 22:38:45 +0100

    Adapt template in preparation for Flexpad developmment

--- src/app.rs
+++ src/app.rs
@@ -1,116 +1,97 @@
+use egui::{ColorImage, TextureHandle};
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
-pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    // this how you opt-out of serialization of a member
+pub struct FlexpadApp {
     #[serde(skip)]
-    value: f32,
+    icon: Option<TextureHandle>,
+    #[serde(skip)]
+    version: &'static str,
+    #[serde(skip)]
+    git_branch: &'static str,
+    #[serde(skip)]
+    git_sha: &'static str,
 }
 
-impl Default for TemplateApp {
+impl Default for FlexpadApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            icon: None,
+            version: "unknown",
+            git_branch: "unknown",
+            git_sha: "unknown",
         }
     }
 }
 
-impl TemplateApp {
-    /// Called once before the first frame.
+impl FlexpadApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // This is also where you can customize the look and feel of egui using
-        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+        cc.egui_ctx.set_visuals(egui::Visuals::light());
 
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
+        let mut result: FlexpadApp;
         if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
+            result = eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        } else {
+            result = Default::default();
+        };
 
-        Default::default()
+        result.version = env!("CARGO_PKG_VERSION");
+        result.git_branch = env!("VERGEN_GIT_BRANCH");
+        result.git_sha = env!("VERGEN_GIT_SHA");
+        result
+    }
+
+    fn render_front_page(&mut self, ctx: &egui::Context) {
+        let product = if self.git_branch == self.version {
+            format!("Flexpad {} ({})", self.version, &self.git_sha[0..7])
+        } else {
+            format!(
+                "Flexpad {} ({} {})",
+                self.version,
+                self.git_branch,
+                &self.git_sha[0..7]
+            )
+        };
+
+        let icon: &TextureHandle = self.icon.get_or_insert_with(|| {
+            load_image_from_memory(
+                include_bytes!("../resources/FlexpadIcon.jpg"),
+                ctx,
+                "FlexpadIcon.jpg",
+            )
+            .expect("FlexpadIcon.jpg not initialized")
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.vertical_centered_justified(|ui| {
+                ui.image(icon, icon.size_vec2());
+                ui.label(product);
+            })
+        });
     }
 }
 
-impl eframe::App for TemplateApp {
-    /// Called by the frame work to save state before shutdown.
+impl eframe::App for FlexpadApp {
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
-    /// Called each time the UI needs repainting, which may be many times per second.
-    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
-
-        // Examples of how to create different panels and windows.
-        // Pick whichever suits you.
-        // Tip: a good default choice is to just keep the `CentralPanel`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
-
-        #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
-            egui::menu::bar(ui, |ui| {
-                ui.menu_button("File", |ui| {
-                    if ui.button("Quit").clicked() {
-                        _frame.close();
-                    }
-                });
-            });
-        });
-
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to(
-                        "eframe",
-                        "https://github.com/emilk/egui/tree/master/crates/eframe",
-                    );
-                    ui.label(".");
-                });
-            });
-        });
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
-        });
-
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally choose either panels OR windows.");
-            });
-        }
+        self.render_front_page(ctx);
     }
+}
+
+fn load_image_from_memory(
+    image_data: &[u8],
+    ctx: &egui::Context,
+    debug_name: &'static str,
+) -> Result<TextureHandle, image::ImageError> {
+    let image = image::load_from_memory(image_data)?;
+    let size = [image.width() as _, image.height() as _];
+    let image_buffer = image.to_rgba8();
+    let pixels = image_buffer.as_flat_samples();
+    let color_image = ColorImage::from_rgba_unmultiplied(size, pixels.as_slice());
+    let texture = ctx.load_texture(debug_name, color_image, Default::default());
+    Ok(texture)
 }
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,17 @@
 #[cfg(not(target_arch = "wasm32"))]
 fn main() -> eframe::Result<()> {
     // Log to stdout (if you run with `RUST_LOG=debug`).
+
     tracing_subscriber::fmt::init();
 
-    let native_options = eframe::NativeOptions::default();
+    let native_options = eframe::NativeOptions {
+        maximized: true,
+        ..Default::default()
+    };
     eframe::run_native(
-        "eframe template",
+        "Flexpad",
         native_options,
-        Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+        Box::new(|cc| Box::new(flexpad::FlexpadApp::new(cc))),
     )
 }
 
@@ -30,7 +34,7 @@ fn main() {
         eframe::start_web(
             "the_canvas_id", // hardcode it
             web_options,
-            Box::new(|cc| Box::new(eframe_template::TemplateApp::new(cc))),
+            Box::new(|cc| Box::new(flexpad::FlexpadApp::new(cc))),
         )
         .await
         .expect("failed to start eframe");
